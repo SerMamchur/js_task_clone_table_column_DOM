396e990ff9f9b3b4efdea315bd1f3c6cbd5bdfb9
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-10 09:19:51 +0100

    make build

---
+++ index.9f497bad.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("thead tr"),t=document.querySelectorAll("tbody tr");function r(e){e.forEach(function(e){var t=e.children[1],r=t.cloneNode(!0),l=t.parentElement,n=l.children[l.children.length-1];l.insertBefore(r,n)})}r(document.querySelectorAll("tfoot tr")),r(e),r(t);
+//# sourceMappingURL=index.9f497bad.js.map
